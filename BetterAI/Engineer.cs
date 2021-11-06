9846407b7df5e7700c0452f74bda709e36d10056
Gustavo Ribeiro <pokegustavo@gmail.com>
2021-11-05 22:11:35 -0300

    No longer changing the engineer spawn location to achive the wanted effect

--- BetterAI/Engineer.cs
+++ BetterAI/Engineer.cs
@@ -5,38 +5,29 @@ namespace BetterAI
 {
     internal class Engineer
     {
-        [HarmonyPatch(typeof(PLServer), "ServerAddCrewBotPlayer")]
-        class SetSpawner 
-        {
-            static void Postfix() 
-            {
-                PLShipInfo ship = PLEncounterManager.Instance.PlayerShip;
-                if(ship != null) 
-                {
-                    GameObject spawner = ship.Spawners[4] as GameObject;
-                    PLUIScreen enginescreen = null;
-                    foreach(PLUIScreen screen in ship.MyScreenBase.AllScreens) 
-                    {
-                        if (screen.name.ToLower().Contains("cloned") && (screen.name.ToLower().Contains("reactor") || screen.name.ToLower().Contains("engineering"))) 
-                        {
-                            enginescreen = screen;
-                            break;
-                        }
-                    }
-                    if(enginescreen != null && spawner != null) 
-                    {
-                        Vector3 down = new Vector3(0, 1, 0);
-                        spawner.transform.position = enginescreen.transform.position + enginescreen.transform.forward - down;
-                    }
-                }
-            }
-        }
         [HarmonyPatch(typeof(PLBot), "TickOptimizeStationAction")]
-        class WarpScreen 
+        class EngineScreens 
         {
             static void Postfix(PLBot __instance) 
             {
-                if(__instance.PlayerOwner.ActiveSubPriority != null) 
+                Vector3 down = new Vector3(0, 1, 0);
+                PLUIScreen enginescreen = null;
+                if (__instance.PlayerOwner.StartingShip == null || __instance.PlayerOwner.GetPawn() == null || __instance.PlayerOwner.GetClassID() != 4) return;
+                foreach (PLUIScreen screen in __instance.PlayerOwner.StartingShip.MyScreenBase.AllScreens)
+                {
+                    if (screen.name.ToLower().Contains("cloned") && (screen.name.ToLower().Contains("reactor") || screen.name.ToLower().Contains("engineering")))
+                    {
+                        enginescreen = screen;
+                        break;
+                    }
+                }
+                if (enginescreen != null)
+                {
+                    __instance.AI_TargetPos = enginescreen.transform.position + enginescreen.transform.forward - down;
+                    __instance.AI_TargetPos_Raw = __instance.AI_TargetPos;
+                    __instance.EnablePathing = true;
+                }
+                if (__instance.PlayerOwner.ActiveSubPriority != null) 
                 {
                     EAIPriorityListDisplayed typeData = (EAIPriorityListDisplayed)__instance.PlayerOwner.ActiveSubPriority.TypeData;
                     if (typeData == EAIPriorityListDisplayed.ENG_JUMP_SHIP || typeData == EAIPriorityListDisplayed.ENG_CHARGE_WARP_DRIVE) 
@@ -52,7 +43,6 @@ namespace BetterAI
                         }
                         if (warpscreen != null)
                         {
-                            Vector3 down = new Vector3(0, 1, 0);
                             __instance.AI_TargetPos = warpscreen.transform.position + warpscreen.transform.forward - down;
                             __instance.AI_TargetPos_Raw = __instance.AI_TargetPos;
                             __instance.EnablePathing = true;
